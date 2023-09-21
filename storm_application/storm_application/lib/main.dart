3c51d592a64dbe5d55c26beb3d9351bc6423818e
Chang Ji-xuan <changjixuan0410@gmail.com>
2023-09-21 09:08:47 +0800

    add-gradle-support

--- storm_application/storm_application/lib/main.dart
+++ storm_application/storm_application/lib/main.dart
@@ -1,24 +1,26 @@
+import 'package:storm_application/firebase_api.dart';
+import 'package:storm_application/page/chatPage.dart';
+import 'package:storm_application/page/chatsPage.dart';
+import 'package:storm_application/users.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:storm_application/login_page.dart';
-import 'main_page.dart';
 
-Future<void> main() async{
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseApi.addRandomUsers(Users.initUsers);
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  static final String title = 'Firebase Chat';
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: title,
+    theme: ThemeData(primarySwatch: Colors.deepOrange),
+    home: ChatsPage(),
+  );
 }
---
+++ storm_application_milestone2/storm_application_milestone2/android/app/src/main/kotlin/com/example/storm_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.storm_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
